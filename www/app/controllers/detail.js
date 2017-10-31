f0572e5cb2987b407b312c45160fa278161a3392
Jose Raul Alfaro Guillen <jralfarog@gmail.com>
2017-10-31 07:16:38 -0600

    d

--- www/app/controllers/detail.js
+++ www/app/controllers/detail.js
@@ -158,6 +158,11 @@ var ft = new FileTransfer();
       $scope.uploadPhotos = function(imageURI) {
         console.log(imageURI);
 
+      if (imageURI.substring(0,21)=="content://com.android") {
+      photo_split=imageURI.split("%3A");
+      imageURI="content://media/external/images/media/"+photo_split[1];
+      }
+      
         $scope.$apply(function () {
              $scope.publicacion.imagen = imageURI;
         $scope.consecutivo = Math.floor(Math.random() * 16) + 1  ;
